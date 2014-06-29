d7f0b3e758310cea80dc2663fb63e657d4a886c1
papilovesyou <chris.cas22@gmail.com>
2014-06-29 04:55:55 -0400

    Update x11.sh

--- x11.sh
+++ x11.sh
@@ -2,7 +2,7 @@
 mine stop
 sleep 5
 cd /opt/miners/
-git clone https://github.com/lasybear/sph-sgminer_x11mod.git sgminer-x11mod
+git clone https://github.com/aznboy84/sgminer sgminer-x11mod
 cd /opt/miners/sgminer-x11mod
 cp /opt/miners/sgminer-4.1.0-sph/ADL_SDK/* /opt/miners/sgminer-x11mod/ADL_SDK/
 make clean
@@ -15,24 +15,24 @@ sleep 5
 make install
 sleep 5
 clear
-cp example.conf /etc/bamt/sgminer-x11mod.conf
+cp example.conf /etc/bamt/aznboy84-x11mod.conf
 cd /etc/bamt/
 patch /etc/bamt/bamt.conf <<.
-115a116
+115a118
 >   cgminer_opts: --api-listen --config /etc/bamt/sgminer-x11mod.conf
 124a126
->   # Sgminer X11 Mod "DRK"
-130a133
->   miner-sgminer-x11mod: 1
+>   # anzboy84 X15 Bitblock "BBL"
+130a134
+>   miner-aznboy84-x11mod: 1
 .
 patch /opt/bamt/common.pl <<.
 1477a1478,1480
->       } elsif (\${\$conf}{'settings'}{'miner-sgminer-x11mod'}) {
+>       } elsif (\${\$conf}{'settings'}{'miner-aznboy84-x11mod'}) {
 >         \$cmd = "cd /opt/miners/sgminer-x11mod/;/usr/bin/screen -d -m -S sgminer-x11 /opt/miners/sgminer-x11mod/sgminer \$args";
 >         \$miner = "sgminer-x11";
 .
 cd /etc/bamt/
-patch /etc/bamt/sgminer-x11mod.conf <<.
+patch /etc/bamt/aznboy84-x11mod.conf <<.
 22c22
 < "kernel" : "ckolivas,ckolivas,ckolivas",
 ---
@@ -47,6 +47,6 @@ patch /etc/bamt/sgminer-x11mod.conf <<.
 > "api-allow": "W:127.0.0.1",
 > 
 .
-echo 'X11Mod Miner Installed.'
+echo 'aznboy84 Miner Installed.'
 echo 'Please review your /etc/bamt/bamt.conf to enable.'
-echo 'Configure /etc/bamt/sgminer-x11mod.conf with pool'
+echo 'Configure /etc/bamt/aznboy84-x11mod.conf with pool'
