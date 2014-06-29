7c56613e510b48c5496a07de5aa187aff5ba652c
g00se01101 <g00se01101@gmail.com>
2014-06-29 16:13:33 -0400

    Update and rename x11.sh to x15.sh

--- x15.sh
+++ x15.sh
@@ -2,9 +2,9 @@
 mine stop
 sleep 5
 cd /opt/miners/
-git clone https://github.com/aznboy84/sgminer sgminer-x11mod
-cd /opt/miners/sgminer-x11mod
-cp /opt/miners/sgminer-4.1.0-sph/ADL_SDK/* /opt/miners/sgminer-x11mod/ADL_SDK/
+git clone https://github.com/aznboy84/sgminer sgminer-x15mod
+cd /opt/miners/sgminer-x15mod
+cp /opt/miners/sgminer-4.1.0-sph/ADL_SDK/* /opt/miners/sgminer-x15mod/ADL_SDK/
 make clean
 sleep 5
 chmod +x autogen.sh
@@ -15,24 +15,24 @@ sleep 5
 make install
 sleep 5
 clear
-cp example.conf /etc/bamt/aznboy84-x11mod.conf
+cp example.conf /etc/bamt/aznboy84-x15mod.conf
 cd /etc/bamt/
 patch /etc/bamt/bamt.conf <<.
 116a117
->   cgminer_opts: --api-listen --config /etc/bamt/aznboy84-x11mod.conf
+>   cgminer_opts: --api-listen --config /etc/bamt/aznboy84-x15mod.conf
 124a127
 >   # anzboy84 X15 Bitblock "BBL"
 129a133
->   miner-aznboy84-x11mod: 1
+>   miner-aznboy84-x15mod: 1
 .
 patch /opt/bamt/common.pl <<.
 1477a1478,1480
->       } elsif (\${\$conf}{'settings'}{'miner-aznboy84-x11mod'}) {
->         \$cmd = "cd /opt/miners/sgminer-x11mod/;/usr/bin/screen -d -m -S sgminer-x11 /opt/miners/sgminer-x11mod/sgminer \$args";
+>       } elsif (\${\$conf}{'settings'}{'miner-aznboy84-x15mod'}) {
+>         \$cmd = "cd /opt/miners/sgminer-x15mod/;/usr/bin/screen -d -m -S sgminer-x15 /opt/miners/sgminer-x15mod/sgminer \$args";
 >         \$miner = "sgminer-x11";
 .
 cd /etc/bamt/
-patch /etc/bamt/aznboy84-x11mod.conf <<.
+patch /etc/bamt/aznboy84-x15mod.conf <<.
 22c22
 < "kernel" : "ckolivas,ckolivas,ckolivas",
 ---
@@ -49,4 +49,4 @@ patch /etc/bamt/aznboy84-x11mod.conf <<.
 .
 echo 'aznboy84 Miner Installed.'
 echo 'Please review your /etc/bamt/bamt.conf to enable.'
-echo 'Configure /etc/bamt/aznboy84-x11mod.conf with pool'
+echo 'Configure /etc/bamt/aznboy84-x15mod.conf with pool'
