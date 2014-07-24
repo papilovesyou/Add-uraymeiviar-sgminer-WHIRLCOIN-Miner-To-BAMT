9cdefbbc1b9cc6cab1fef6a0fda3c4d1327d1111
papilovesyou <chris.cas22@gmail.com>
2014-07-24 04:29:56 -0400

    Update whirlcoin.sh

--- whirlcoin.sh
+++ whirlcoin.sh
@@ -3,13 +3,13 @@ mine stop
 sleep 5
 cp config.patch /tmp/
 cd /opt/miners/
+git submodule update --init --recursive
 git clone https://github.com/uraymeiviar/sgminer sgminer-whirlcoin
 cd /opt/miners/sgminer-whirlcoin
 git checkout whirlcoin
 cp /opt/miners/sgminer-4.1.0-sph/ADL_SDK/* /opt/miners/sgminer-whirlcoin/ADL_SDK/
 make clean
 sleep 5
-git submodule update --init --recursive
 chmod +x autogen.sh
 ./autogen.sh
 sleep 2
